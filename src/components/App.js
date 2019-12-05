5acb9a6c5624ee03bb9a0a9b1e40a7996f37f60c
diffuse92 <lim.bs@ensecure.co.kr>
2019-12-05 09:15:04 +0900

    todo-list

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageTemplate from './PageTemplate';
+
+class App extends React.Component{
+  render(){
+    return (
+      <PageTemplate>안녕하세요</PageTemplate>
+    );
+  }
+}
+
+export default App;
