0c1fc63a1d7357bbc589548e7a98481de3345b0c
limbs@ensecure <lim.bs@ensecure.co.kr>
2019-12-06 17:59:14 +0900

    증간 저장

--- src/components/App.js
+++ src/components/App.js
@@ -4,27 +4,30 @@ import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
 class App extends React.Component{
-constructor(props){
-  super(props);
-  this.state = {
-    input: '',
-    todos:[
-      { id:0, text: 'Study React', done: true},
-      { id:1, text: 'Styling Component', done: false}
-    ]
-  };
+// constructor(props){
+//   super(props);
 
-  let id = 1;
-  const getId = () => {
-    return ++this.id;
-  }
-}
+// }
+
+state = {
+  input: '',
+  todos:[
+    { id:0, text: 'Study React', done: true},
+    { id:1, text: 'Styling Component', done: false}
+  ],
+};
+
+//일정 데이터 안에 들어가는 id 값
+id = 1
+getId = () => {
+  return ++id;
+};
 
 handleChange = (e) => {
   const { value } = e.target;
   this.setState({
     input: value
-  })
+  });
 };
 
 //데이터 추가
@@ -35,13 +38,33 @@ handleInsert = () => {
   const newTodo = {
     text : input,
     done : false,
-    id: this.getId()
-  }
+    id: getId()
+  };
 
   //setState 로 데이터 등록
   this.setState({
     todos:[...todos, newTodo],
     input: ''
+  });
+};
+
+handleToggle = (id) => {
+  //id로 배열 인덱스 찾기
+  const { todos } = this.state;
+  const index = todos.findIndex(todo => todo.id === id);
+
+  //찾은 데이터 'done' 값 반전시키기
+  const toogled = {
+    ...todos[index],
+    done: !todos[index].done
+  };
+
+  this.setState({
+    todos: [
+      ...todos.slice(0, index),
+      toogled,
+      ...todos.slice(index + 1, todos.length)
+    ]
   })
 }
 
@@ -49,13 +72,14 @@ handleInsert = () => {
     const { input, todos } = this.state;
     const {
       handleChange,
-      handleInsert
+      handleInsert,
+      handleToggle
     } = this;
 
     return (
       <PageTemplate>
         <TodoInput onChange={handleChange} onInsert={handleInsert} value={input}/>
-        <TodoList todos={todos}/>
+        <TodoList todos={todos} onToggle={handleToggle}/>
       </PageTemplate>
     );
   }
